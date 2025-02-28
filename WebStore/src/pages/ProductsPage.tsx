2e56ab439634104c4bd2f037f9b45761994c0ff9
Gadza03 <mgadza@pmfst.hr>
2025-02-28 21:00:04 +0100

    Fix logic and start with product single page

--- WebStore/src/pages/ProductsPage.tsx
+++ WebStore/src/pages/ProductsPage.tsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
 import { Product } from "../types/Product";
 import { fetchProducts } from "../api";
 import ProductsList from "../components/ProductsList";
 
+const PRODUCT_ITEMS = "products";
+
 export default function ProductPage() {
   const [products, setProducts] = useState<Product[]>([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetchProducts();
-      const data = await response;
-      setProducts(data);
-    };
+    const localProducts = JSON.parse(
+      localStorage.getItem(PRODUCT_ITEMS) || "[]"
+    ) as Product[];
 
-    fetchData();
+    if (localProducts.length > 0) {
+      setProducts(localProducts);
+    } else {
+      fetchProducts()
+        .then((data) => {
+          setProducts(data);
+        })
+        .catch((error) => console.error("Error fetching products:", error));
+    }
   }, []);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      localStorage.setItem(PRODUCT_ITEMS, JSON.stringify(products));
+    }
+  }, [products]);
 
   return <ProductsList products={products} />;
 }
